59a2f4a842d50089330dd554f45f65614a35c4bf
Kalyani Mahajan <https://github.com/06Pseudo06>
2026-03-31 21:07:02 +0530

    Backend core setup + auth system implemented

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const testRoutes = require('./routes/test.routes');
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/test', testRoutes);
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api/user', userRoutes);
+
+
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+module.exports = app;
+
+
+
